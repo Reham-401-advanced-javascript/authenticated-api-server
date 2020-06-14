b3ebc66c5fb27dd8bf116d315046b954ad2350c5
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-14 14:51:48 +0300

    still test

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const { server } = require('../src/server.js');
+const { server } = require('../lib/server.js');
 const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(server);
 
